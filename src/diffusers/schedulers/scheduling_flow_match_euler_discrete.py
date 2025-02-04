5a7ab0d868838a1faced0bb8519d736d15918f99
linoytsaban <linoy@huggingface.co>
2025-02-04 23:18:51 +0200

    fix scheduler

--- src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
+++ src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
@@ -473,5 +473,53 @@ class FlowMatchEulerDiscreteScheduler(SchedulerMixin, ConfigMixin):
         )
         return sigmas
 
+    def step_forward(
+            self,
+            model_output: torch.FloatTensor,
+            timestep: Union[float, torch.FloatTensor],
+            sample: torch.FloatTensor,
+            s_churn: float = 0.0,
+            s_tmin: float = 0.0,
+            s_tmax: float = float("inf"),
+            s_noise: float = 1.0,
+            generator: Optional[torch.Generator] = None,
+            return_dict: bool = True,
+    ) -> Union[FlowMatchEulerDiscreteSchedulerOutput, Tuple]:
+        """
+        Forward invrsion step
+        """
+
+        if (
+                isinstance(timestep, int)
+                or isinstance(timestep, torch.IntTensor)
+                or isinstance(timestep, torch.LongTensor)
+        ):
+            raise ValueError(
+                (
+                    "Passing integer indices (e.g. from `enumerate(timesteps)`) as timesteps to"
+                    " `EulerDiscreteScheduler.step()` is not supported. Make sure to pass"
+                    " one of the `scheduler.timesteps` as a timestep."
+                ),
+            )
+
+        if self.step_index is None:
+            self._init_step_index(timestep)
+
+        # Upcast to avoid precision issues when computing prev_sample
+        sample = sample.to(torch.float32)
+
+        sigma = self.sigmas[self.step_index]
+        sigma_next = self.sigmas[self.step_index + 1]
+
+        next_sample = sample + (sigma - sigma_next) * model_output
+
+        # Cast sample back to model compatible dtype
+        next_sample = next_sample.to(model_output.dtype)
+
+        # upon completion increase step index by one
+        self._step_index -= 1
+
+        return (next_sample,)
+
     def __len__(self):
         return self.config.num_train_timesteps
