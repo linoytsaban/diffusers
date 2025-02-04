763b81c9960b10023b039f13c25917dd7082d5dd
linoytsaban <linoy@huggingface.co>
2025-02-04 12:41:17 +0200

    import fix

--- src/diffusers/pipelines/kolors/pipeline_kolors_leditspp.py
+++ src/diffusers/pipelines/kolors/pipeline_kolors_leditspp.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import inspect
+import math
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import torch
